f56428c2d9181ecfdb00f84cadf39c551fb46d3e
Ronan <85257187+ronan-s1@users.noreply.github.com>
2022-02-20 00:24:38 +0100

    new file struct

---
+++ commands.py
@@ -0,0 +1,70 @@
+import requests
+from bs4 import BeautifulSoup
+import random
+
+
+#reads facts.txt and returns a list
+def facts_and_gif(f):
+	my_file = open(f, "r")
+	content = my_file.read()
+	content_list = content.split("\n")
+	my_file.close()
+	
+	return random.choice(content_list)
+
+
+# scrapes the met.ie webpage for todays weather
+def getting_weather():
+	page = requests.get("https://www.met.ie/forecasts/dublin")
+	soup = BeautifulSoup(page.content, "html.parser")
+
+	title = soup.select("h2")[0].text
+	description = soup.select("p")[3].text
+
+	return f"**{title}**\n{description}"
+
+
+# gets tomorrow's weather
+def tomorrow():
+	page = requests.get("https://www.met.ie/forecasts/dublin")
+	soup = BeautifulSoup(page.content, "html.parser")
+
+	for count, i in enumerate(soup.select("h2")):
+		if "TOMORROW" in i.text:
+			title = i.text
+			break
+
+	description = soup.select("p")[3 + count].text
+
+	return f"**{title}**\n{description}"
+
+
+# links to articles
+def news():
+	# news_string = "**Weather Related News:**\n"
+	news_list = []
+	page = requests.get("https://www.google.com/search?q=weather+ireland&rlz=1C1ONGR_en-GBIE979IE979&biw=1536&bih=842&tbm=nws&sxsrf=APq-WBsl_-eeWzTn7FGhUDdCzlFHF_pROg%3A1645226936889&ei=uCsQYubSNePA8gLQq7TABg&ved=0ahUKEwjmjpf5s4r2AhVjoFwKHdAVDWgQ4dUDCA0&uact=5&oq=weather+ireland&gs_lcp=Cgxnd3Mtd2l6LW5ld3MQAzIKCAAQsQMQgwEQQzILCAAQsQMQgwEQkQIyCwgAELEDEIMBEJECMgsIABCxAxCDARCRAjILCAAQgAQQsQMQgwEyCwgAEIAEELEDEIMBMgsIABCABBCxAxCDATIICAAQsQMQgwEyCwgAEIAEELEDEIMBMgsIABCABBCxAxCDAVAAWMwRYJwTaABwAHgAgAHSAYgBxQiSAQYxNC4wLjGYAQCgAQHAAQE&sclient=gws-wiz-news")
+
+	soup = BeautifulSoup(page.content, "html.parser")
+
+	# finds all classes with kCrYT
+	for item in soup.find_all("div", attrs={"class" : "kCrYT"}):
+		# gets the links only
+		raw_link = (item.find("a", href=True)["href"])
+
+		# removes evrything after &sa=U&, making the link useable
+		link = "<" + (raw_link.split("/url?q=")[1]).split("&sa=U&")[0] + ">"
+
+		news_list.append(link)
+	
+	return news_list
+
+
+# shows list of commands
+def commands():
+	commands = ["**!commands** - shows list of commands (this)", "**!weather** - shows the current weather of Dublin", "**!tmr** - shows the weather for tomorrow in Dublin","**!news** - shows weather related articles in Ireland","**!fact** - shows a fun weather fact", "**!gif** - shows a weather gif"]
+	result = ""
+	for command in commands:
+		result += command + "\n"
+
+	return result
--- main.py
+++ main.py
@@ -1,79 +1,9 @@
 import discord
-import requests
 import os
 import asyncio
-from bs4 import BeautifulSoup
 import datetime
-import random
+import commands
 from keep_alive import keep_alive
-
-
-#reads facts.txt and returns a list
-def facts_and_gif(f):
-	my_file = open(f, "r")
-	content = my_file.read()
-	content_list = content.split("\n")
-	my_file.close()
-	
-	return random.choice(content_list)
-
-
-# scrapes the met.ie webpage for todays weather
-def getting_weather():
-	page = requests.get("https://www.met.ie/forecasts/dublin")
-	soup = BeautifulSoup(page.content, "html.parser")
-
-	title = soup.select("h2")[0].text
-	description = soup.select("p")[3].text
-
-	return f"**{title}**\n{description}"
-
-
-# gets tomorrow's weather
-def tomorrow():
-	page = requests.get("https://www.met.ie/forecasts/dublin")
-	soup = BeautifulSoup(page.content, "html.parser")
-
-	for count, i in enumerate(soup.select("h2")):
-		if "TOMORROW" in i.text:
-			title = i.text
-			break
-
-	description = soup.select("p")[3 + count].text
-
-	return f"**{title}**\n{description}"
-
-
-# links to articles
-def news():
-	# news_string = "**Weather Related News:**\n"
-	news_list = []
-	page = requests.get("https://www.google.com/search?q=weather+ireland&rlz=1C1ONGR_en-GBIE979IE979&biw=1536&bih=842&tbm=nws&sxsrf=APq-WBsl_-eeWzTn7FGhUDdCzlFHF_pROg%3A1645226936889&ei=uCsQYubSNePA8gLQq7TABg&ved=0ahUKEwjmjpf5s4r2AhVjoFwKHdAVDWgQ4dUDCA0&uact=5&oq=weather+ireland&gs_lcp=Cgxnd3Mtd2l6LW5ld3MQAzIKCAAQsQMQgwEQQzILCAAQsQMQgwEQkQIyCwgAELEDEIMBEJECMgsIABCxAxCDARCRAjILCAAQgAQQsQMQgwEyCwgAEIAEELEDEIMBMgsIABCABBCxAxCDATIICAAQsQMQgwEyCwgAEIAEELEDEIMBMgsIABCABBCxAxCDAVAAWMwRYJwTaABwAHgAgAHSAYgBxQiSAQYxNC4wLjGYAQCgAQHAAQE&sclient=gws-wiz-news")
-
-	soup = BeautifulSoup(page.content, "html.parser")
-
-	# finds all classes with kCrYT
-	for item in soup.find_all("div", attrs={"class" : "kCrYT"}):
-		# gets the links only
-		raw_link = (item.find("a", href=True)["href"])
-
-		# removes evrything after &sa=U&, making the link useable
-		link = "<" + (raw_link.split("/url?q=")[1]).split("&sa=U&")[0] + ">"
-
-		news_list.append(link)
-	
-	return news_list
-
-
-# shows list of commands
-def commands():
-	commands = ["**!commands** - shows list of commands (this)", "**!weather** - shows the current weather of Dublin", "**!tmr** - shows the weather for tomorrow in Dublin","**!news** - shows weather related articles in Ireland","**!fact** - shows a fun weather fact", "**!gif** - shows a weather gif"]
-	result = ""
-	for command in commands:
-		result += command + "\n"
-
-	return result
-
 
 # --- discord side ---
 client = discord.Client()
@@ -91,7 +21,7 @@ async def daily_weather():
 
 		if wait_time == 0:
 			channel = client.get_channel(893033706107858945)
-			daily_message = "**DAILY UPDATE:**\n" + getting_weather()
+			daily_message = "**DAILY UPDATE:**\n" + commands.getting_weather()
 			await channel.send(daily_message)
 			wait_time = 1
 
@@ -106,34 +36,36 @@ async def on_ready():
 #if user does a command
 @client.event
 async def on_message(message):
+	facts_file = "other/facts.txt"
+	gifs_file = "other/gifs.txt"
 	msg = message.content.lower()
 	if message.author == client.user:
 		return
 
 	#send weather
 	elif msg.startswith("!weather"):
-		await message.channel.send(getting_weather())
+		await message.channel.send(commands.getting_weather())
 	
 	#shows list of commands
 	elif msg.startswith("!commands"):
-		await message.channel.send(commands())
+		await message.channel.send(commands.commands())
 
 	#shows fact
 	elif msg.startswith("!fact"):
-		await message.channel.send(facts_and_gif("facts.txt"))
+		await message.channel.send(commands.facts_and_gif(facts_file))
 	
 	#shows gif
 	elif msg.startswith("!gif"):
-		await message.channel.send(facts_and_gif("gifs.txt"))
+		await message.channel.send(commands.facts_and_gif(gifs_file))
 	
 	#send tomorrow's weather
 	elif msg.startswith("!tmr"):
-		await message.channel.send(tomorrow())
+		await message.channel.send(commands.tomorrow())
 	
 	#send tomorrow's weather
 	elif msg.startswith("!news"):
 		#calling news function to get articles
-		articles = news()
+		articles = commands.news()
 		articles = [link for i, link in enumerate(articles) if i % 2 == 1]
 		count = 0
 		await message.channel.send("**Weather Related News Articles: (be patient)**\n")
